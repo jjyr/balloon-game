e80729f95a3328e21d1cd6249d2bc85ac979a301
jjy <jjyruby@gmail.com>
2024-08-19 15:21:15 +0800

    commit 1

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use std::{
     fs,
 };
 
-use glam::UVec2;
-use roast_2d::prelude::*;
+use glam::{IVec2, UVec2};
+use roast_2d::{ldtk::LdtkProject, prelude::*};
 
 const ACCEL_DEFLATION: f32 = 900.0;
 const ACCEL_GROUND: f32 = 600.0;
@@ -14,11 +14,12 @@ const FRICTION_GROUND: f32 = 2.;
 const FRICTION_AIR: f32 = 2.;
 const JUMP_HIGH_TIME: f32 = 0.08;
 const JUMP_HIGH_ACCEL: f32 = 780.0;
-const INFLATION_SPEED: f32 = 0.7;
+const INFLATION_SPEED: f32 = 1.2;
+const MIN_INFLATION: f32 = 0.3;
+const MAX_INFLATION: f32 = 8.;
+const PLAYER_SIZE: Vec2 = Vec2::new(32.0, 32.0);
 
 const SPRITE_SIZE: f32 = 8.0;
-const BRICK_SIZE: Vec2 = Vec2::new(64., 32.);
-const BRICK_DYING: f32 = 0.3;
 const TEXTURE_PATH: &str = "assets/images/demo.png";
 const LEVEL_PATH: &str = "game.ldtk";
 const VIEW_SIZE: Vec2 = Vec2::new(512.0, 512.0);
@@ -36,9 +37,15 @@ fn load_texture(eng: &mut Engine) -> Image {
     })
 }
 
+fn lerp_size(ori_size: Vec2, inflation_rate: f32) -> Vec2 {
+    (ori_size * MAX_INFLATION) * ((inflation_rate) / MAX_INFLATION).powi(2)
+}
+
 #[derive(Default)]
 pub struct Game {
-    pub score: usize,
+    pub dead: usize,
+    pub ldtk_project: LdtkProject,
+    pub current_level: usize,
 }
 
 #[repr(u8)]
@@ -59,67 +66,80 @@ impl From<Action> for ActionId {
 }
 
 #[derive(Default, Clone)]
-pub struct Brick {
-    hit: bool,
-    dying: f32,
-    dead_pos: Vec2,
+pub struct Spikes;
+
+impl EntityType for Spikes {
+    fn init(&mut self, eng: &mut Engine, ent: &mut Entity) {
+        ent.size = Vec2::new(32., 10.);
+        let mut sheet = load_texture(eng);
+        sheet.scale = ent.size / SPRITE_SIZE;
+        sheet.color = RED;
+        ent.anim = Some(Animation::new(sheet));
+        ent.check_against = EntityGroup::PLAYER;
+        ent.physics = EntityPhysics::FIXED;
+        ent.gravity = 0.;
+    }
+    fn touch(&mut self, eng: &mut Engine, ent: &mut Entity, other: &mut Entity) {
+        eng.kill(other.ent_ref);
+    }
 }
 
-impl EntityType for Brick {
+#[derive(Default, Clone)]
+pub struct Button;
+
+impl EntityType for Button {
+    fn init(&mut self, eng: &mut Engine, ent: &mut Entity) {
+        ent.size = Vec2::new(32., 32.);
+        let mut sheet = load_texture(eng);
+        sheet.scale = ent.size / SPRITE_SIZE;
+        sheet.color = PURPLE;
+        ent.anim = Some(Animation::new(sheet));
+        ent.check_against = EntityGroup::PLAYER;
+        ent.physics = EntityPhysics::FIXED;
+        ent.gravity = 0.;
+    }
+    fn touch(&mut self, eng: &mut Engine, ent: &mut Entity, other: &mut Entity) {
+        let mut spikes = Vec::new();
+        for ent in eng.world().entities() {
+            let Ok(ent) = ent.try_borrow() else {
+                continue;
+            };
+            if ent.ent_type.is::<Spikes>() {
+                spikes.push(ent.ent_ref);
+            }
+        }
+
+        for ent_ref in spikes {
+            eng.kill(ent_ref);
+        }
+    }
+}
+
+#[derive(Default, Clone)]
+pub struct Door;
+
+impl EntityType for Door {
     fn init(&mut self, eng: &mut Engine, ent: &mut Entity) {
         let mut sheet = load_texture(eng);
-        sheet.scale = BRICK_SIZE / SPRITE_SIZE;
+        sheet.scale = Vec2::new(32., 32.) / SPRITE_SIZE;
         sheet.color = Color::rgb(0x5b, 0x6e, 0xe1);
         ent.anim = Some(Animation::new(sheet));
-        ent.size = BRICK_SIZE;
-        ent.check_against = EntityGroup::PROJECTILE;
-        ent.physics = EntityPhysics::ACTIVE;
+        ent.group = EntityGroup::PICKUP;
+        ent.check_against = EntityGroup::PLAYER;
+        ent.physics = EntityPhysics::FIXED;
+        ent.size = Vec2::new(32., 32.);
+        ent.gravity = 0.;
     }
-
-    fn kill(&mut self, _eng: &mut Engine, _ent: &mut Entity) {
+    fn touch(&mut self, eng: &mut Engine, ent: &mut Entity, other: &mut Entity) {
         G.with_borrow_mut(|g| {
-            g.score += 1;
+            let next_level = g.current_level + 1;
+            let level = format!("Level_{}", next_level);
+            if let Err(err) = eng.load_level(&g.ldtk_project, &level) {
+                eprintln!("Can't load level {level} err {err:?}");
+            } else {
+                g.current_level = next_level;
+            }
         });
-    }
-
-    fn update(&mut self, eng: &mut Engine, ent: &mut Entity) {
-        if self.hit {
-            self.dying += eng.tick;
-            if self.dying > BRICK_DYING {
-                eng.kill(ent.ent_ref);
-            }
-
-            if let Some(anim) = ent.anim.as_mut() {
-                let progress = (self.dying / BRICK_DYING).powi(2);
-                let color = {
-                    let (r1, g1, b1): (u8, u8, u8) = (0x5b, 0x6e, 0xe1);
-                    let (r2, g2, b2) = (RED.r, RED.g, RED.b);
-                    let r = r1.saturating_add(((r1 as f32 - r2 as f32) * progress).abs() as u8);
-                    let g = g1.saturating_add(((g1 as f32 - g2 as f32) * progress).abs() as u8);
-                    let b = b1.saturating_add(((b1 as f32 - b2 as f32) * progress).abs() as u8);
-                    Color::rgb(r, g, b)
-                };
-                let scale = {
-                    let start = 1.0;
-                    let end = start * 0.5;
-                    start - (start - end) * progress
-                };
-                let size = BRICK_SIZE * scale;
-                let center_pos = self.dead_pos + BRICK_SIZE * 0.5;
-                ent.pos = center_pos - size * 0.5;
-                ent.size = size;
-
-                anim.sheet.scale = size / SPRITE_SIZE;
-                anim.sheet.color = color;
-            }
-        }
-    }
-
-    fn touch(&mut self, _eng: &mut Engine, ent: &mut Entity, _other: &mut Entity) {
-        if !self.hit {
-            self.hit = true;
-            self.dead_pos = ent.pos;
-        }
     }
 }
 
@@ -130,28 +150,28 @@ pub struct Player {
     inflation_rate: f32,
     original_size: Vec2,
     normal: Vec2,
+    inflation: f32,
 }
 
 impl EntityType for Player {
     fn init(&mut self, eng: &mut Engine, ent: &mut Entity) {
         let mut sheet = load_texture(eng);
-        ent.size = Vec2::new(32.0, 32.0);
-        sheet.scale = ent.size / SPRITE_SIZE;
         sheet.color = Color::rgb(0x37, 0x94, 0x6e);
-        ent.anim = Some(Animation::new(sheet));
         ent.check_against = EntityGroup::PROJECTILE;
         ent.physics = EntityPhysics::ACTIVE;
         ent.group = EntityGroup::PLAYER;
         ent.gravity = 1.0;
         ent.mass = 1.0;
-        // ent.restitution = 1.0;
-        self.original_size = Vec2::new(32.0, 32.0);
-        self.inflation_rate = 1.0;
+        self.original_size = PLAYER_SIZE;
+        self.inflation_rate = 3.0;
         self.normal.x = 1.;
+        ent.size = lerp_size(PLAYER_SIZE, self.inflation_rate);
+        sheet.scale = ent.size / SPRITE_SIZE;
+        ent.anim = Some(Animation::new(sheet));
 
         // set camera
         let cam = eng.camera_mut();
-        cam.follow(ent.ent_ref, false);
+        cam.follow(ent.ent_ref, true);
         cam.speed = 3.;
         cam.min_vel = Vec2::splat(5.);
     }
@@ -166,20 +186,72 @@ impl EntityType for Player {
             FRICTION_AIR
         };
 
-        let mut deflation = false;
-
-        if input.pressed(&Action::Inflate.into()) {
-            self.inflation_rate += INFLATION_SPEED * eng.tick;
-        } else if input.pressed(&Action::Deflate.into()) {
-            self.inflation_rate -= INFLATION_SPEED * eng.tick;
-            deflation = true;
+        let inflation;
+        if input.pressed(Action::Inflate) && self.inflation_rate < MAX_INFLATION {
+            inflation = 1.;
+        } else if input.pressed(Action::Deflate) && self.inflation_rate > MIN_INFLATION {
+            inflation = -1.;
+        } else {
+            inflation = 0.;
         }
-        ent.size = self.original_size * self.inflation_rate;
-        ent.mass = 1.0 / self.inflation_rate;
-        ent.gravity = 1.0 / self.inflation_rate;
+
+        // collision detect
+        // 1. calc new pos, size...
+        // 2. check collision
+        // 3. cancel infliction if not possible
+        if inflation != 0.0 {
+            let inflation_rate = (self.inflation_rate + inflation * INFLATION_SPEED * eng.tick)
+                .clamp(MIN_INFLATION, MAX_INFLATION);
+            // let size = self.original_size * inflation_rate;
+            let size = lerp_size(self.original_size, inflation_rate);
+            // let old_size = self.original_size * self.inflation_rate;
+            let old_size = lerp_size(self.original_size, self.inflation_rate);
+            let pos = (size - old_size).ceil() * Vec2::new(-0.5, -1.0) + ent.pos;
+            dbg!(size, old_size, ent.pos, pos);
+            // TODO check collition
+
+            let mut collision = false;
+            if let Some(map) = eng.collision_map.as_ref() {
+                let tile_pos = {
+                    let pos = pos / map.tile_size;
+                    IVec2::new(pos.x as i32, pos.y as i32)
+                };
+                let corner_tile_pos = {
+                    dbg!("corner", pos + size);
+                    let pos = (pos + size) / map.tile_size;
+                    IVec2::new(pos.x as i32, pos.y as i32)
+                };
+                dbg!(corner_tile_pos);
+                'outer: for y in tile_pos.y..=corner_tile_pos.y {
+                    for x in tile_pos.x..=corner_tile_pos.x {
+                        if !map.get(IVec2::new(x, y)).is_some_and(|v| v == 0) {
+                            collision = true;
+                            break 'outer;
+                        }
+                    }
+                }
+            }
+
+            // do inflation
+            if collision {
+                return;
+            }
+            self.inflation_rate = inflation_rate;
+            self.inflation = inflation;
+            ent.size = size;
+            ent.pos = pos;
+            ent.mass = (1.0 / self.inflation_rate).clamp(0.1, 1.0);
+            ent.gravity = (1.0 / self.inflation_rate).clamp(0.1, 1.0);
+            // Scale sprite image
+            if let Some(anim) = ent.anim.as_mut() {
+                anim.sheet.scale = ent.size / SPRITE_SIZE;
+            }
+        } else {
+            self.inflation = 0.;
+        }
 
         let mut normal = self.normal;
-        if input.pressed(&Action::Right.into()) {
+        if input.pressed(Action::Right) {
             ent.accel.x = if ent.on_ground {
                 ACCEL_GROUND
             } else {
@@ -187,7 +259,7 @@ impl EntityType for Player {
             };
             self.normal.x = 1.0;
             normal.x = 1.0
-        } else if input.pressed(&Action::Left.into()) {
+        } else if input.pressed(Action::Left) {
             ent.accel.x = -if ent.on_ground {
                 ACCEL_GROUND
             } else {
@@ -199,10 +271,10 @@ impl EntityType for Player {
             normal.x = 0.0;
         }
 
-        if input.pressed(&Action::Up.into()) {
+        if input.pressed(Action::Up) {
             self.normal.y = -1.0;
             normal.y = -1.0
-        } else if input.pressed(&Action::Down.into()) {
+        } else if input.pressed(Action::Down) {
             self.normal.y = 1.0;
             normal.y = 1.0
         } else {
@@ -214,11 +286,11 @@ impl EntityType for Player {
             normal = self.normal;
         }
 
-        if deflation {
+        if self.inflation < 0. {
             ent.accel += normal * ACCEL_DEFLATION;
         }
 
-        if input.pressed(&Action::Jump.into()) {
+        if input.just_pressed(Action::Jump) {
             if ent.on_ground && self.can_jump {
                 ent.vel.y = -PLAYER_JUMP_VEL;
                 self.can_jump = false;
@@ -236,14 +308,17 @@ impl EntityType for Player {
             self.high_jump_time = 0.;
             self.can_jump = true;
         }
+    }
 
-        // TODO Check collision
-        // TODO push ent if collision
-        if let Some(anim) = ent.anim.as_mut() {
-            anim.sheet.scale = self.original_size / SPRITE_SIZE * self.inflation_rate;
-        }
-
-        eng.entity_base_update(ent);
+    fn kill(&mut self, eng: &mut Engine, _ent: &mut Entity) {
+        eprintln!("Player dead... reload level");
+        G.with_borrow_mut(|g| {
+            g.dead += 1;
+            let level = format!("Level_{}", g.current_level);
+            if let Err(err) = eng.load_level(&g.ldtk_project, &level) {
+                eprintln!("Can't load level {level} err {err:?}");
+            }
+        });
     }
 }
 
@@ -275,17 +350,17 @@ impl Scene for Demo {
 
         // bind keys
         let input = eng.input_mut();
-        input.bind(KeyCode::Left, Action::Left.into());
-        input.bind(KeyCode::Right, Action::Right.into());
-        input.bind(KeyCode::KeyA, Action::Left.into());
-        input.bind(KeyCode::KeyD, Action::Right.into());
-        input.bind(KeyCode::Up, Action::Up.into());
-        input.bind(KeyCode::KeyW, Action::Up.into());
-        input.bind(KeyCode::Down, Action::Down.into());
-        input.bind(KeyCode::KeyS, Action::Down.into());
-        input.bind(KeyCode::Space, Action::Jump.into());
-        input.bind(KeyCode::KeyI, Action::Inflate.into());
-        input.bind(KeyCode::KeyO, Action::Deflate.into());
+        input.bind(KeyCode::Left, Action::Left);
+        input.bind(KeyCode::Right, Action::Right);
+        input.bind(KeyCode::KeyA, Action::Left);
+        input.bind(KeyCode::KeyD, Action::Right);
+        input.bind(KeyCode::Up, Action::Up);
+        input.bind(KeyCode::KeyW, Action::Up);
+        input.bind(KeyCode::Down, Action::Down);
+        input.bind(KeyCode::KeyS, Action::Down);
+        input.bind(KeyCode::Space, Action::Jump);
+        input.bind(KeyCode::KeyI, Action::Inflate);
+        input.bind(KeyCode::KeyO, Action::Deflate);
 
         // TODO the font path only works on MacOS
         let font_path = "/Library/Fonts/Arial Unicode.ttf";
@@ -295,12 +370,11 @@ impl Scene for Demo {
             eprintln!("Failed to load font from {font_path}");
         }
 
-        let proj = {
-            let content = fs::read(LEVEL_PATH).unwrap();
-            serde_json::from_slice(&content).unwrap()
-        };
         eng.gravity = 400.0;
-        eng.load_level(&proj, "Level_0").unwrap();
+        G.with_borrow(|g| {
+            let level = format!("Level_{}", g.current_level);
+            eng.load_level(&g.ldtk_project, &level).unwrap();
+        });
     }
 
     fn update(&mut self, eng: &mut Engine) {
@@ -319,8 +393,8 @@ impl Scene for Demo {
             }
         }
         if let Some(font) = self.font.clone() {
-            let score = G.with_borrow(|g| g.score);
-            let content = format!("Score: {}", score);
+            let score = G.with_borrow(|g| g.dead);
+            let content = format!("Deads: {}", score);
             let text = Text::new(content, font.clone(), 30.0, WHITE);
             self.score_text = eng.create_text_texture(text).ok();
         }
@@ -339,6 +413,15 @@ impl Scene for Demo {
 }
 
 fn main() {
+    G.with_borrow_mut(|g| {
+        g.dead = 0;
+        g.current_level = 1;
+        g.ldtk_project = {
+            let content = fs::read(LEVEL_PATH).unwrap();
+            serde_json::from_slice(&content).unwrap()
+        };
+    });
+
     let mut eng = Engine::new();
     // set resize and scale
     eng.set_view_size(VIEW_SIZE);
@@ -349,7 +432,9 @@ fn main() {
     });
     eng.set_sweep_axis(SweepAxis::Y);
     eng.add_entity_type::<Player>();
-    eng.add_entity_type::<Brick>();
+    eng.add_entity_type::<Door>();
+    eng.add_entity_type::<Spikes>();
+    eng.add_entity_type::<Button>();
     eng.set_scene(Demo::default());
     if let Err(err) = run(
         eng,
